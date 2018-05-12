f4e791289582087518af55ebf3d11947fcb5370b
MeltyPlayer <mastergamepro@hotmail.com>
2018-05-12 06:01:24 -0400

    Fixed some constant names.

--- RetroRecompiler/src/stages/asm/gb/color_defs.h
+++ RetroRecompiler/src/stages/asm/gb/color_defs.h
@@ -3,8 +3,8 @@
 
 namespace recom::bin::gb {
   const fin::graphics::Color
-    BLACK = fin::graphics::Color::from_rgb_b(0, 0, 0),
-    DARK_GRAY = fin::graphics::Color::from_rgb_b(96, 96, 96),
-    LIGHT_GRAY = fin::graphics::Color::from_rgb_b(192, 192, 192),
-    WHITE = fin::graphics::Color::from_rgb_b(255, 255, 255);
+    kBlack = fin::graphics::Color::from_rgb_b(0, 0, 0),
+    kDarkGray = fin::graphics::Color::from_rgb_b(96, 96, 96),
+    kLightGray = fin::graphics::Color::from_rgb_b(192, 192, 192),
+    kWhite = fin::graphics::Color::from_rgb_b(255, 255, 255);
 }
